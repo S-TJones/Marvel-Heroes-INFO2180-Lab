60f290b998d8ec128db918343f14745ae425d586
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2019-11-04 04:28:46 +0000

    Add superheroes.php starter code

---
+++ superheroes.php
@@ -0,0 +1,54 @@
+<?php
+
+$superheroes = [
+  [
+      "id" => 1,
+      "name" => "Steve Rogers",
+      "alias" => "Captain America",
+  ],
+  [
+      "id" => 2,
+      "name" => "Tony Stark",
+      "alias" => "Ironman",
+  ],
+  [
+      "id" => 3,
+      "name" => "Peter Parker",
+      "alias" => "Spiderman",
+  ],
+  [
+      "id" => 4,
+      "name" => "Carol Danvers",
+      "alias" => "Captain Marvel",
+  ],
+  [
+      "id" => 5,
+      "name" => "Natasha Romanov",
+      "alias" => "Black Widow",
+  ],
+  [
+      "id" => 6,
+      "name" => "Bruce Banner",
+      "alias" => "Hulk",
+  ],
+  [
+      "id" => 7,
+      "name" => "Clint Barton",
+      "alias" => "Hawkeye",
+  ],
+  [
+      "id" => 8,
+      "name" => "T'challa",
+      "alias" => "Black Panther",
+  ],
+  [
+      "id" => 9,
+      "name" => "Thor",
+      "alias" => "Thor",
+  ],
+  [
+      "id" => 10,
+      "name" => "Wanda Maximoff",
+      "alias" => "Scarlett Witch",
+  ], 
+];
