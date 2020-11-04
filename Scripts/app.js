58ae1cff4b66093530840a5feeeed33b810523fd
Shemar Jones <47797339+S-TJones@users.noreply.github.com>
2020-11-03 23:17:48 -0500

    Exercise 2 - halfway
    About halfway done with Exercise 2. Added the onclick function. All that's left is to do the ajax request.

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,25 @@
 // Driver function
 var driver = function(){
 
+    // Gets the button
+    var btn = document.getElementById("search");
+
+    // Runs function when button is clicked
+    btn.onclick = function(){
+        // Alerts
+        alert("Hello! I am an alert box!!");
+
+        // Ajax call/ Ajax request
+        $.ajax({
+            url: "./superheroes.php",
+            type: "GET",
+            dataType: "html",
+            success: function (data) {
+                // Not sure what should go here
+                // $('#container').html(data);
+            }
+        });
+    }
 };
 
 // Runs this function on startup
